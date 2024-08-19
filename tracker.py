06061f82c2117694e06283b32b7a03df8949e6ce
Magooch <crvallecito@gmail.com>
2024-08-18 19:17:02 -0600

    Set program to accept user input and create 4 default functions to be worked on later

--- tracker.py
+++ tracker.py
@@ -3,5 +3,9 @@ print("1. Add an Expense")
 print("2. View Expenses")
 print("3. Calculate Total Expenses")
 print("4. Exit\n")
+print(input("Please choose an option (1-4): "))
 
-print("Please choose an option (1-4):")
+def add_expense():
+def view_expenses():
+def calculate_expenses():
+def exit_program():
