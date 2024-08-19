f242b1a86a48664ab683a404a6a6c325cc4922bc
Magooch <crvallecito@gmail.com>
2024-08-18 19:13:50 -0600

    Add expense tracker layout

---
+++ tracker.py
@@ -0,0 +1,7 @@
+print("Welcome to the Simple Expense Tracker\n")
+print("1. Add an Expense")
+print("2. View Expenses")
+print("3. Calculate Total Expenses")
+print("4. Exit\n")
+
+print("Please choose an option (1-4):")
