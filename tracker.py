02bb0abbab04a2ca297a6cdd8587904d512164c5
Magooch <crvallecito@gmail.com>
2024-08-18 20:30:23 -0600

    Add functionality to add expenses to tracker, view expenses, calculate the total of expenses and exit the program

--- tracker.py
+++ tracker.py
@@ -5,7 +5,18 @@ print("3. Calculate Total Expenses")
 print("4. Exit\n")
 print(input("Please choose an option (1-4): "))
 
+expenses = []
+
 def add_expense():
+    expenses.append(input.value)
+
 def view_expenses():
+    print(expenses)
+
 def calculate_expenses():
+    for expense in expenses:
+        total = expense + expense
+    print(f"The total of your expenses comes out to: {total}")
+
 def exit_program():
+    exit
