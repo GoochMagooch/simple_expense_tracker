32f88e04acc822a45be635c759f9d280a768072a
Magooch <crvallecito@gmail.com>
2024-08-19 10:03:37 -0600

    Stored user input in variable 'choice' and modified logic in 'calculate_expenses()'

--- tracker.py
+++ tracker.py
@@ -3,32 +3,31 @@ print("1. Add an Expense")
 print("2. View Expenses")
 print("3. Calculate Total Expenses")
 print("4. Exit\n")
-print(input("Please choose an option (1-4): "))
-
+choice = print(input("Please choose an option (1-4): "))
 expenses = []
 
 def add_expense():
-    print(input("Enter your expense: "))
-    expenses.append(input.value)
+    my_expense = print(input("Enter your expense: "))
+    expenses.append(my_expense)
 
 def view_expenses():
     print(expenses)
 
 def calculate_expenses():
     for expense in expenses:
-        total = expense + expense
+        total += expense
     print(f"The total of your expenses comes out to: {total}")
 
 def exit_program():
     exit
 
-if input == 1:
+if choice == 1:
     add_expense()
-elif input == 2:
+elif choice == 2:
     view_expenses()
-elif input == 3:
+elif choice == 3:
     calculate_expenses()
-elif input == 4:
+elif choice == 4:
     exit_program()
 else:
     print("Please enter a number from 1 to 4...")
