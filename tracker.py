e608fd5f7e1aaf8c18cf1e561d4a08ab12b7e7d5
Magooch <crvallecito@gmail.com>
2025-05-18 14:02:28 -0700

    Refactor add_expense try/except; Fix logical error with view_expenses; Refactor calculate_expenses to read from files; back option for add_expense during both add expense and add amount prompts

--- tracker.py
+++ tracker.py
@@ -4,178 +4,183 @@ import csv
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-# Adds/updates Expenses in expenses.csv
-def add_expense():
-    # Separates expenses file into expense and amount
+def display_menu():
+    print("1. Add an expenses")
+    print("2. View expensess")
+    print("3. Calculate Total expensess")
+    print("\"Menu\" to bring up menu")
+    print("\"Exit\" to exit\n")
+
+# Adds/updates expensess in expenses.csv
+def add_expenses():
+    # Separates expensess file into expenses and amounts
     with open("expenses.csv") as x:
         expenses_file = csv.reader(x, delimiter=',')
-        expense = []
-        amount = []
+        expenses = []
+        amounts = []
         for i in expenses_file:
-            expense.append(i[0])
-            amount.append(float(i[1]))
+            expenses.append(i[0])
+            amounts.append(float(i[1]))
 
-    # Defines list of formatted expenses
+    # Defines list of formatted expensess
     formatted_expenses = []
     if os.path.getsize("expenses.csv") == 0:
-        print("No logged expenses! Add some!")
+        print("No logged expensess! Add some!")
     else:
         print("YOUR EXPENSES")
-        for i in range(len(expense)):
-            if len(str(amount[i])[str(amount[i]).index(".")+1:]) == 1 and str(amount[i])[str(amount[i]).index(".")+1:] == "0":
-                formatted_expenses.append(f"{i+1}. {expense[i]} - ${amount[i]:.2f}")
-            elif len(str(amount[i])[str(amount[i]).index(".")+1:]) == 1:
-                formatted_expenses.append(f"{i+1}. {expense[i]} - ${amount[i]:.2f}")
-            elif len(str(amount[i])[str(amount[i]).index(".")+1:]) == 2:
-                formatted_expenses.append(f"{i+1}. {expense[i]} - ${amount[i]}")
+        for i in range(len(expenses)):
+            if len(str(amounts[i])[str(amounts[i]).index(".")+1:]) == 1 and str(amounts[i])[str(amounts[i]).index(".")+1:] == "0":
+                formatted_expenses.append(f"{i+1}. {expenses[i]} - ${amounts[i]:.2f}")
+            elif len(str(amounts[i])[str(amounts[i]).index(".")+1:]) == 1:
+                formatted_expenses.append(f"{i+1}. {expenses[i]} - ${amounts[i]:.2f}")
+            elif len(str(amounts[i])[str(amounts[i]).index(".")+1:]) == 2:
+                formatted_expenses.append(f"{i+1}. {expenses[i]} - ${amounts[i]}")
 
     # Holds numbers to run against user choice
     num_list = []
     for i in range(len(formatted_expenses)):
-        num_list.append(formatted_expenses[i][0])
+        num_list.append(int(formatted_expenses[i][0]))
 
-    # Prints full add expense menu
-    print("0. Add new expense")
+    # Prints full add expenses menu
+    print("0. Add new expenses")
     for i in formatted_expenses:
         print(i)
 
-    # Adds/updates expenses
+    # Adds/updates expensess
     while True:
+        clear_screen()
+        print("0. Add new expenses")
+        for i in formatted_expenses:
+            print(i)
         exp_choice_int = input("Choose a number or \"back\": ")
-        try:
-            if exp_choice_int in num_list:
-                ex_amount = float(input("Enter the price of your expense: "))
-                for i in range(len(expense)):
-                    if exp_choice_int == num_list[i]:
-                        amount[i] = amount[i] + ex_amount
-                open("expenses.csv", "w").close()
-                for i in range(len(expense)):
+        if exp_choice_int.lower() == "back":
+            clear_screen()
+            break
+        elif exp_choice_int == "0":
+            my_expenses = input("Describe your expense or enter \"back\": ")
+            if my_expenses.lower() == "back":
+                pass
+            else:
+                ex_amount = input("Enter expense amount or enter \"back\": ")
+                if ex_amount.lower() == "back":
+                    pass
+                else:
                     with open("expenses.csv", "a", newline="") as x:
                         expenses_file = csv.writer(x)
-                        expenses_file.writerow([expense[i], amount[i]])
+                        expenses_file.writerow([my_expenses, float(ex_amount)])
+                    clear_screen()
+                    print(f"Expense added!")
+                    break
+        else:
+            try:
+                int_choice = int(exp_choice_int)
+                # add conditional for "back"
+                if int_choice in num_list:
+                    ex_amount = float(input("Enter the price of your expenses: "))
+                    for i in range(len(expenses)):
+                        if int_choice == num_list[i]:
+                            amounts[i] = amounts[i] + ex_amount
+                    open("expenses.csv", "w").close()
+                    for i in range(len(expenses)):
+                        with open("expenses.csv", "a", newline="") as x:
+                            expenses_file = csv.writer(x)
+                            expenses_file.writerow([expenses[i], amounts[i]])
+                    clear_screen()
+                    print(f"Expense updated!")
+                    break
+                else:
+                    clear_screen()
+                    print("Enter a number or \"back\"")
+                    pass
+            except ValueError:
                 clear_screen()
-                print(f"Expense amount updated!")
-                break
-            elif exp_choice_int == "0":
-                my_expense = input("Describe your expense: ")
-                ex_amount = float(input("Enter price of your expense: "))
-                with open("expenses.csv", "a", newline="") as x:
-                    expenses_file = csv.writer(x)
-                    expenses_file.writerow([my_expense, ex_amount])
-                clear_screen()
-                print(f"Expense and amount added to expense list!")
-                break
-            elif exp_choice_int.lower() == "back":
-                clear_screen()
-                break
-            else:
-                clear_screen()
-                print("Error: Invalid selection")
+                print("Enter a number or \"back\"")
                 pass
-        except ValueError:
-            clear_screen()
-            print("Enter a number or \"back\"")
-            pass
 
-# Lists all expenses
+# Lists all expensess
 def view_expenses():
     with open("expenses.csv") as x:
         expenses_file = csv.reader(x, delimiter=',')
-        expense = []
-        amount = []
+        expenses = []
+        amounts = []
         for i in expenses_file:
-            expense.append(i[0])
-            amount.append(float(i[1]))
-    for i in range(len(expense)):
-        if len(str(amount[i])[str(amount[i]).index(".")+1:]) == 1 and str(amount[i])[str(amount[i]).index(".")+1:] == "0":
-            print(f"{i+1}. {expense[i]} - ${amount[i]:.2f}")
-            break
-        elif len(str(amount[i])[str(amount[i]).index(".")+1:]) == 1:
-            print(f"{i+1}. {expense[i]} - ${amount[i]:.2f}")
-            break
-        elif len(str(amount[i])[str(amount[i]).index(".")+1:]) == 2:
-            print(f"{i+1}. {expense[i]} - ${amount[i]}")
-            break
+            expenses.append(i[0])
+            amounts.append(float(i[1]))
+    
+    if os.path.getsize("expenses.csv") == 0:
+        clear_screen()
+        print("No logged expenses! Add some by choosing option 1!\n")
+    else:
+        for i in range(len(expenses)):
+            if len(str(amounts[i])[str(amounts[i]).index(".")+1:]) == 1 and str(amounts[i])[str(amounts[i]).index(".")+1:] == "0":
+                print(f"{i+1}. {expenses[i]} - ${amounts[i]:.2f}")
+            elif len(str(amounts[i])[str(amounts[i]).index(".")+1:]) == 1:
+                print(f"{i+1}. {expenses[i]} - ${amounts[i]:.2f}")
+            else:
+                print(f"{i+1}. {expenses[i]} - ${amounts[i]}")
+
+def calculate_expenses():
+    total = 0
+    # Separates expensess file into expenses and amounts
+    with open("expenses.csv") as x:
+        expenses_file = csv.reader(x, delimiter=',')
+        amounts = []
+        for i in expenses_file:
+            amounts.append(float(i[1]))
+
+    if os.path.getsize("expenses.csv") == 0:
+        clear_screen()
+        print("No logged expenses! Add some by choosing option 1!\n")
+    else:
+        for amount in amounts:
+            total += amount
+
+        if len(str(total)[str(total).index(".")+1:]) == 1 and str(total)[str(total).index(".")+1:] == "0":
+            print(f"The total of your expensess comes out to: ${int(total)}")
+        elif len(str(total)[str(total).index(".")+1:]) == 1:
+            print(f"The total of your expensess comes out to: ${total:.2f}")
+        else:
+            print(f"The total of your expensess comes out to: ${total}")
 
 def exit_program():
-    print("Exiting Expense Tracker...Goodbye")
+    print("Exiting expenses Tracker...Goodbye")
     exit()
 
-print("Welcome to the Simple Expense Tracker\n")
-print("1. Add an Expense")
-print("2. View Expenses")
-print("3. Calculate Total Expenses")
-print("\"Menu\" to bring up menu")
-print("\"Exit\" to exit\n")
+print("Welcome to the Simple expenses Tracker\n")
 
 while True:
+    display_menu()
+
     choice = input("Please choose an option (1 - 3), \"menu\", or \"exit\": ")
 
     if choice.lower() == "exit":
         print("Are you sure you want to exit?")
         exit_choice = input("Press \"Y\" or \"N\": ")
         if exit_choice.lower() == "y":
+            clear_screen()
             exit_program()
         else:
             clear_screen()
             pass
     elif choice.lower() == "menu":
         clear_screen()
-        print("1. Add an Expense")
-        print("2. View Expenses")
-        print("3. Calculate Total Expenses")
-        print("\"Menu\" to bring up menu")
-        print("\"Exit\" to exit\n")
     else:
         try:
             int_choice = int(choice)
             if int_choice == 1:
                 clear_screen()
-                add_expense()
+                add_expenses()
             elif int_choice == 2:
                 clear_screen()
                 view_expenses()
-            '''elif int_choice == 3:
+            elif int_choice == 3:
                 clear_screen()
                 calculate_expenses()
             else:
                 clear_screen()
                 print("Choice not found...")
-                pass'''
+                pass
         except ValueError:
             clear_screen()
             print("Choice not found...")
             pass
-
-'''
-def view_expenses():
-    if len(expenses) == 0:
-        print("There are no expenses in your tracker! Add some by pressing 1")
-    else:
-        for key,value in expenses.items():
-            if len(str(value)[str(value).index(".")+1:]) == 1 and str(value)[str(value).index(".")+1:] == "0":
-                print(f"{key}: ${int(value)}")
-            elif len(str(value)[str(value).index(".")+1:]) == 1:
-                print(f"{key}: ${value:.2f}")
-            elif len(str(value)[str(value).index(".")+1:]) == 2:
-                print(f"{key}: ${value}")
-            else:                
-                print(f"{key}: ${int(value)}")
-
-def calculate_expenses():
-    total = 0
-    if len(expenses) == 0:
-        print("There are no expenses in your tracker! Add some by pressing 1")
-    else:
-        for expense in expenses.values():
-            total += expense
-
-        if len(str(total)[str(total).index(".")+1:]) == 1 and str(total)[str(total).index(".")+1:] == "0":
-            print(f"The total of your expenses comes out to: ${int(total)}")
-        elif len(str(total)[str(total).index(".")+1:]) == 1:
-            print(f"The total of your expenses comes out to: ${total:.2f}")
-        elif len(str(total)[str(total).index(".")+1:]) == 2:
-            print(f"The total of your expenses comes out to: ${total}")
-        else:
-            print(f"The total of your expenses comes out to: ${int(total)}")
-'''
